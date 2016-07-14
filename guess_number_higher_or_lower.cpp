98e31a60e5c990bfd3ca7151a16d5520f762d241
周炀 <zhoudayang2@163.com>
2016-07-15 00:08:25 +0800

    add note

--- guess_number_higher_or_lower.cpp
+++ guess_number_higher_or_lower.cpp
@@ -6,18 +6,25 @@ int guess(int num);
 class Solution {
 public:
     int guessNumber(int n) {
+        // first test number n
         if(guess(n)==0)
             return n;
+        //init left and right
         int l=1,r=n;
         while(l<r){
+            //注意需要采取这种方式求mid，如果mid=(l+r)/2，容易越界
             int mid = l+((r-l)>>1);
+            //test mid
             if(guess(mid)==0)
                 return mid;
+            //less than the number, in the right part
             else if(guess(mid)==1)
                 l=mid;
+            //bigger than the number, int the left part
             else if(guess(mid)==-1)
                 r=mid;
         }
+        //if left and right meet, return l
         return l;
     }
 };
