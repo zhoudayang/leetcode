689f4de88db24afbb1615f715192022f3a10c7c4
zhoudayang <zhoudayang2@163.com>
2016-11-23 11:14:03 +0800

    modify shortest-palindorme.cc

--- shortest-palindrome.cc
+++ shortest-palindrome.cc
@@ -1,3 +1,15 @@
+//基本的思路时找出从位置0开始的最长回文字符串，然后在字符串开头加上调转字符串，且长度为原字符串长度减去最长回文字符串的长度。但是此法无法执行，超时！
+//为此可以把原问题转换为求一个字符串的最长前缀后缀。
+//我们需要添加字符之后最短的回文数。为此构建一个字符串，设原来的字符串为s，将s字符串的转置字符串设为reverse_str,
+//构建字符串s+"#"+reverse_str,求新字符串的最长前缀后缀，设其长度为common_len。最后，只需要在原字符串前面添加reverse_str的前面
+//s.size()-common_len个字符即可。
+//举例子：
+/*
+  origin_str : aacecaaa
+  connection_str: aacecaaa#aaacecaa
+  max_fix_str : aacecaa
+  所以需要在orgin_str前面添加a字符
+*/
 class Solution{
 public:
     string shortestPalindrome(string s){
